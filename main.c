39c6837d76529c8166897cbeac371ba2a96be5d6
C.S. Melis <45081813+BC46@users.noreply.github.com>
2025-03-12 23:02:16 +0100

    Fix BOM removal not working if _tmp file already exists

--- main.c
+++ main.c
@@ -32,6 +32,7 @@ int wmain(int argc, wchar_t *argv[]) {
     wcscpy(newName, filePath);
     wcscat(newName, L"_tmp");
 
+    DeleteFile(newName);
     _wrename(filePath, newName);
 
     fileIn = _wfopen(newName, L"r");
